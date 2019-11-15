39407f8270577bcdee7731063a0ef5c833120f1f
Alexander Nytofte Markussen <alexander.markussen@live.dk>
2019-11-15 06:14:17 +0100

    Add project files.

---
+++ TODOListApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TODOListApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
